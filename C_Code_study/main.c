8228caacf03d4469349deecf30a6d5fafaaad7e1
JJLee <ljjj5646@gmail.com>
2021-07-12 07:46:29 +0900

    Initial Commit

---
+++ C_Code_study/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_Code_study
+//
+//  Created by JJLee on 2021/07/12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
